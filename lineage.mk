676e1582f9279d78d9926aa61097f17c3fb730df
Sandeep Sethi <adeveloper79@gmail.com>
2017-05-29 22:29:31 +0530

    lineage.mk: Typo

--- lineage.mk
+++ lineage.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from AOSP product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(RC_TARGET_DIR)/product/telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 
 # Inherit Lineage's custom product configuration
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
